903be4cc02e37a439a785bb793512ce233650536
Troels Ugilt Jensen <tujensen@gmail.com>
2016-11-10 14:20:26 +0100

    Issue #BIB-140: Added comment

--- src/middleware/AbstractTagReader.java
+++ src/middleware/AbstractTagReader.java
@@ -2,6 +2,15 @@ package middleware;
 
 import java.util.ArrayList;
 
+/**
+ * AbstractTagReader.
+ * 
+ * Supplies basic logic for a TagReader, with a Thread run-method that reads/writes tags according
+ * to which state it is in.
+ * 
+ * This class should be extended for a new TagReader, and at least the abstract methods
+ * need to be overridden.
+ */
 public abstract class AbstractTagReader extends Thread implements TagReaderInterface {
 	protected Boolean connected = false;
 	protected Boolean running = false;
